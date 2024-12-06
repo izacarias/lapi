9d25fc873dcb5f00ed5af3e28002037e992ca5b4
izacarias <iuli.zacarias+git@gmail.com>
2024-12-06 17:14:27 +0100

    debugging zone query problem

--- controllers/zone_controller.go
+++ controllers/zone_controller.go
@@ -1,34 +1,44 @@
 package controllers
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 	"github.com/izacarias/lapi/configs"
+	"github.com/izacarias/lapi/models"
+	"github.com/izacarias/lapi/responses"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var zoneCollection *mongo.Collection = configs.GetCollection(configs.DB, "zones")
-var validate = validator.New()
 
-func CreateZone() gin.HandlerFunc {
+func GetZone() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		// var zone models.Zone
-		// defer cancel()
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		zoneId := c.Param("id")
+		var zone models.Zone
+		defer cancel()
 
-		// if err := c.ShouldBindJSON(&zone); err != nil {
-		// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		// 	return
-		// }
-		// if err := validate.Struct(zone); err != nil {
-		// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		// 	return
-		// }
-		// result, err := zoneCollection.InsertOne(context.Background(), zone)
-		// if err != nil {
-		// 	c.JSON(http.StatusInternalServerError, gin.H{"error": "Error creating zone"})
-		// 	return
-		// }
-		// c.JSON(http.StatusCreated, result)
+		objId, _ := zoneCollection.Find(ctx, bson.M{})
+
+		err := zoneCollection.FindOne(ctx, bson.M{"id": objId}).Decode(&zone)
+
+		if err != nil {
+			log.Printf("error getting zone %s: %v", zoneId, err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching zone"})
+			return
+		}
+		// TODO: Process Zone Information here before returning
+		c.JSON(http.StatusOK, responses.ZoneResponse{
+			ZoneId:                            zone.Id,
+			NumberOfAccessPoints:              0,
+			NumberOfUnserviceableAccessPoints: 0,
+			NumberOfUsers:                     0,
+			ResourceURL:                       "http://localhost:8080/api/v1/zone/" + zone.Id,
+		})
 	}
 }
--- routes/zone_route.go
+++ routes/zone_route.go
@@ -1,7 +1,11 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/izacarias/lapi/controllers"
+)
 
 func ZoneRoute(router *gin.Engine) {
 	// Routes related to zones
+	router.GET("/zone/:id", controllers.GetZone())
 }
