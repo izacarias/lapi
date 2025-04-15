af022c0a73c3e14013871c891473d65b1d3afe73
izacarias <iuli.zacarias+git@gmail.com>
2025-04-15 23:09:43 +0200

    Refactoring user controller

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -62,95 +63,128 @@ func ListUsers() gin.HandlerFunc {
 
 		// qsUserAddress is not empty
 		if len(qsUserAddress) > 0 {
-			// filter by user address
-			userList := make([]domain.User, 0)
-			for _, userAddress := range qsUserAddress {
-				user, err := services.GetUserByAddress(userAddress)
-				if err != nil {
-					c.String(http.StatusInternalServerError, "error getting user by address: %s", err)
-					return
-				}
-				userList = append(userList, *user)
-			}
+			userList := getUsersByAddresses(qsUserAddress)
 
-			// filter by Zone
-			if len(qsZones) > 0 && len(qsApId) == 0 {
-				// if qsZones is not empty, only add users with matching zoneId
-				userListFiltered := make([]domain.User, 0)
-				for _, zoneId := range qsZones {
-					for _, user := range userList {
-						if user.ZoneId == zoneId {
-							userListFiltered = append(userListFiltered, user)
-						}
-					}
-				}
-				userList = userListFiltered
-			}
+			// Apply filters based on query parameters
+			userList = applyFilters(userList, qsZones, qsApId)
 
-			// filter by Access Point
-			if len(qsZones) == 0 && len(qsApId) > 0 {
-				// if qsApId is not empty, only add users with matching accessPointId
-				userListFiltered := make([]domain.User, 0)
-				for _, accessPointId := range qsApId {
-					for _, user := range userList {
-						if user.AccessPoint == accessPointId {
-							userListFiltered = append(userListFiltered, user)
-						}
-					}
-				}
-				userList = userListFiltered
-			}
-			// filter by both Zone and Access Point
-			if len(qsZones) > 0 && len(qsApId) > 0 {
-				// if both qsZones and qsApId are not empty, only add users with matching zoneId and accessPointId
-				userListFiltered := make([]domain.User, 0)
-				for _, zoneId := range qsZones {
-					for _, accessPointId := range qsApId {
-						for _, user := range userList {
-							if user.ZoneId == zoneId && user.AccessPoint == accessPointId {
-								userListFiltered = append(userListFiltered, user)
-							}
-						}
-					}
-				}
-				userList = userListFiltered
-			}
-
-			// return the user list as JSON
-			usersResponse := make([]responses.UserInfo, 0)
-			for _, user := range userList {
-				usersResponse = append(usersResponse, responses.UserInfo{
-					Address:       user.Address,
-					AccessPointId: &user.AccessPoint,
-					ZoneId:        user.ZoneId,
-					ResourceURL:   utils.GetUserResourceUrl(c.Request, user.Address),
-					//TODO: Update with the timestamp from the last location update
-					Timestamp: responses.TimeStamp{Seconds: uint32(100), NanoSeconds: uint32(200)},
-					//TODO: Update with the location info
-					LocationInfo: &responses.LocationInfo{Latitude: []float32{43.123456}, Longitude: []float32{7.123456}, Altitude: 0.000, Shape: responses.LocationInfoShapeN2},
-				})
-			}
-			response := responses.UserInfoList{
-				ResourceURL: utils.GetUserResourceUrl(c.Request, qsUserAddress[0]),
-				User:        usersResponse,
-			}
+			// Convert to response format and return
+			response := formatUserResponse(c, userList, qsUserAddress[0])
 			c.JSON(http.StatusOK, response)
 			return
-
 		} else {
+			userList, err := services.GetAllUsers()
+			if err != nil {
+				log.Printf("Error retrieving all users: %v", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve users"})
+				return
+			}
 			// TODO: filter by zoneId and/or accessPointId
-			if len(qsZones) > 0 && len(qsApId) == 0 {
-				// get all users in the zone
-			}
-			if len(qsZones) == 0 && len(qsApId) > 0 {
-				// get all users in the access point
-			}
-			if len(qsZones) > 0 && len(qsApId) > 0 {
-				// get all users in the zone and filter for users in the provided access point list
-				//TODO: get all users in the zone and filter for users in the provided access point list
+			userList = applyFilters(userList, qsZones, qsApId)
+			// Convert to response format and return
+			response := formatUserResponse(c, userList, "")
+			c.JSON(http.StatusOK, response)
+			return
+		}
+	}
+}
+
+// getUsersByAddresses retrieves users by their addresses
+func getUsersByAddresses(addresses []string) []domain.User {
+	userList := make([]domain.User, 0)
+	for _, userAddress := range addresses {
+		user, err := services.GetUserByAddress(userAddress)
+		if err != nil {
+			// Note: This should be handled better - possibly returning an error
+			// Currently preserving the original behavior
+			log.Printf("Error retrieving user by address %s: %v", userAddress, err)
+			continue
+		}
+		userList = append(userList, *user)
+	}
+	return userList
+}
+
+// applyFilters applies zone and access point filters to the user list
+func applyFilters(userList []domain.User, qsZones []string, qsApId []string) []domain.User {
+	// filter by Zone only
+	if len(qsZones) > 0 && len(qsApId) == 0 {
+		return filterByZone(userList, qsZones)
+	}
+
+	// filter by Access Point only
+	if len(qsZones) == 0 && len(qsApId) > 0 {
+		return filterByAccessPoint(userList, qsApId)
+	}
+
+	// filter by both Zone and Access Point
+	if len(qsZones) > 0 && len(qsApId) > 0 {
+		return filterByZoneAndAccessPoint(userList, qsZones, qsApId)
+	}
+
+	// No filters applied
+	return userList
+}
+
+// filterByZone filters users by zone IDs
+func filterByZone(userList []domain.User, zoneIds []string) []domain.User {
+	userListFiltered := make([]domain.User, 0)
+	for _, zoneId := range zoneIds {
+		for _, user := range userList {
+			if user.ZoneId == zoneId {
+				userListFiltered = append(userListFiltered, user)
 			}
 		}
+	}
+	return userListFiltered
+}
 
-		c.String(http.StatusOK, "zoneId: %s, accessPointId: %s, userAddress: %s", qsZones, qsApId, qsUserAddress)
+// filterByAccessPoint filters users by access point IDs
+func filterByAccessPoint(userList []domain.User, accessPointIds []string) []domain.User {
+	userListFiltered := make([]domain.User, 0)
+	for _, accessPointId := range accessPointIds {
+		for _, user := range userList {
+			if user.AccessPoint == accessPointId {
+				userListFiltered = append(userListFiltered, user)
+			}
+		}
+	}
+	return userListFiltered
+}
+
+// filterByZoneAndAccessPoint filters users by both zone and access point IDs
+func filterByZoneAndAccessPoint(userList []domain.User, zoneIds []string, accessPointIds []string) []domain.User {
+	userListFiltered := make([]domain.User, 0)
+	for _, zoneId := range zoneIds {
+		for _, accessPointId := range accessPointIds {
+			for _, user := range userList {
+				if user.ZoneId == zoneId && user.AccessPoint == accessPointId {
+					userListFiltered = append(userListFiltered, user)
+				}
+			}
+		}
+	}
+	return userListFiltered
+}
+
+// formatUserResponse converts domain users to response format
+func formatUserResponse(c *gin.Context, userList []domain.User, queryAddress string) responses.UserInfoList {
+	usersResponse := make([]responses.UserInfo, 0)
+	for _, user := range userList {
+		usersResponse = append(usersResponse, responses.UserInfo{
+			Address:       user.Address,
+			AccessPointId: &user.AccessPoint,
+			ZoneId:        user.ZoneId,
+			ResourceURL:   utils.GetUserResourceUrl(c.Request, user.Address),
+			//TODO: Update with the timestamp from the last location update
+			Timestamp: responses.TimeStamp{Seconds: uint32(100), NanoSeconds: uint32(200)},
+			//TODO: Update with the location info
+			LocationInfo: &responses.LocationInfo{Latitude: []float32{43.123456}, Longitude: []float32{7.123456}, Altitude: 0.000, Shape: responses.LocationInfoShapeN2},
+		})
+	}
+
+	return responses.UserInfoList{
+		ResourceURL: utils.GetUserResourceUrl(c.Request, queryAddress),
+		User:        usersResponse,
 	}
 }
