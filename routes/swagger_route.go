7ff1954673de3a6ccf489922df4af90ebb78e57c
izacarias <i.zacarias@tu-braunschweig.de>
2025-05-06 12:00:51 +0200

    Updating Swagger configuration

--- routes/swagger_route.go
+++ routes/swagger_route.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	// Swagger docs
+	"github.com/izacarias/lapi/configs"
 	_ "github.com/izacarias/lapi/docs" // This line is necessary for go-swagger to find your docs!
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -10,6 +11,10 @@ import (
 
 func SwaggerRoute(router *gin.Engine) {
 	// Routes related to Swagger
-	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json")
+	rootUrl := configs.GetConfigApiRoot()
+	url := ginSwagger.URL(rootUrl + "/swagger/doc.json")
+	router.GET("/docs", func(c *gin.Context) {
+		c.Redirect(302, "/swagger/index.html")
+	})
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 }
